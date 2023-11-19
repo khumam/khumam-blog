bff9b2391d148242a46c37b04da8ca64ab6f7eb3
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-11-19 15:33:42 +0700

    up: fix error

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,11 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'Manuskrip Kesunyian',
+    description:
+      'Manuskrip Kesunyian adalah sebuah blog arsip yang berisi berbagai gagasan dari sang penulis',
+    imgSrc: '/static/images/google.png',
+    href: 'https://blog.khumam.com',
+  },
+]
 
 export default projectsData
